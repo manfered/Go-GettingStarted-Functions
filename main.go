450074a173a2d746ad945fe3c04a01f728e4e255
manfered <farzad.seifi@gmail.com>
2022-02-06 15:02:35 -0700

    functions with multiple return values and also using WriteOnly variables

--- main.go
+++ main.go
@@ -11,8 +11,15 @@ func main() {
 	result2 := startServer2(9000)
 	fmt.Println(result2)
 
-	errorResult := startServer3(10000)
-	fmt.Println(errorResult)
+	errorResult3 := startServer3(10000)
+	fmt.Println(errorResult3)
+
+	result4, errorResult4 := startServer4(11000)
+	fmt.Println(result4, errorResult4)
+
+	// writeonly variable if we do not need to use the variable in order to prevent compilation error
+	_, errorResult4_1 := startServer4(12000)
+	fmt.Println(errorResult4_1)
 }
 
 // function with parameter but no return value
@@ -33,4 +40,11 @@ func startServer3(port int) error {
 	fmt.Println("Server3 is starting...")
 	fmt.Println("Server3 failed on port", port)
 	return errors.New("something went wrong")
+}
+
+// function with parameter return multiple values
+func startServer4(port int) (bool, error) {
+	fmt.Println("Server3 is starting...")
+	fmt.Println("Server3 failed on port", port)
+	return true, nil
 }
